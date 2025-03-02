b47aaf33c50cf76e7b2c00bceae87228cd83d821
jjyuhub <jyu290@student.vu.nl>
2025-03-02 15:01:30 +0100

    Create cpu_module.cpp

---
+++ cpu_module.cpp
@@ -0,0 +1,8 @@
+// File 4: cpu_module.cpp
+#include <systemc.h>
+SC_MODULE(CPU_Module) {
+    sc_in<bool> Port_CLK;
+    SC_CTOR(CPU_Module) {
+        cout << "[CPU] Module constructed." << endl;
+    }
+};
